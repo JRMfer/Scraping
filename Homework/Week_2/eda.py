63ed616e34cd585130782eb3552e697b7384b21b
JRMfer <43999123+JRMfer@users.noreply.github.com>
2018-11-14 16:06:02 +0100

    almost ready

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -15,87 +15,124 @@ import json
 INPUT_CSV = "input.csv"
 
 
-def csv_reader(filename):
+def csv_reader(filename, columns):
+    """
+    Loads csv file as dataframe and select columns
+    to analyze
+    """
+
     data = pd.read_csv(filename)
-    data = data[["Country", "Region", "GDP ($ per capita) dollars",
-                 "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)"]]
-    data = data.replace("unknown", np.nan)
-    data = data.dropna(how='any')
+    data = data[columns]
     return data
 
-# je wilt over elke rij itereren en kijken of de rij NaN bevat. Zo ja, verwijder
-# de rij van de data frame aangezien deze niet van nut kan zijn
 
+def remove_string(data, column, string):
+    """
+    Strips entire column dataframe of string (only rightside)
+    """
 
-def remove_string(data_frame, column, string):
-    data_frame[column] = data_frame[column].map(lambda x: x.rstrip(string))
-    return data_frame
+    data[column] = data[column].map(lambda x: x.rstrip(string))
+    return data
 
 
 def string_to_numeric(data, column):
+    """
+    Convert all strings in column to numeric
+    """
     data[column] = pd.to_numeric(data[column])
     return data
 
 
 def make_float(data, column):
+    """
+    Convert all decimals with commas in column
+    to decimals with dots and finally to a float
+    """
     data[column] = [x.replace(',', '.') for x in data[column]]
-    return data
-
-
-def rem_white(data, column):
-    data[column] = [x.rstrip() for x in data[column]]
+    data = string_to_numeric(data, column)
     return data
 
 
 def preprocessing(data):
+    """
+    Removes incomplete observations and
+    makes all values in columns usable
+    """
+
+    data = data.replace("unknown", np.nan)
+    data = data.dropna(how='any')
     data = remove_string(
         data, "GDP ($ per capita) dollars", "dollars")
     data = string_to_numeric(data, "GDP ($ per capita) dollars")
     data = make_float(data, "Pop. Density (per sq. mi.)")
     data = make_float(data, "Infant mortality (per 1000 births)")
-    data = string_to_numeric(data, "Pop. Density (per sq. mi.)")
-    data = string_to_numeric(
-        data, "Infant mortality (per 1000 births)")
-    data = rem_white(data, "Region")
-    data = data[["Country", "Region", "GDP ($ per capita) dollars",
-                 "Pop. Density (per sq. mi.)",
-                 "Infant mortality (per 1000 births)"]]
+    data = remove_string(data, "Region", " ")
     return data
 
 
-def central_tendency(data, column, xlabel):
-    data = remove_outliers(data, column)
-    data.hist(column)
-    plt.title(column, fontsize=16)
-    plt.xlabel(xlabel, fontsize=14)
-    plt.ylabel("Frequency", fontsize=14)
-    plt.show()
-    return
+def central_tendency(data, column, xlabel, ylabel):
+    """
+    Calculates mean, median, mode and standard deviation.
+    Also plots a histogram with outliers and without outliers
+    """
 
-def remove_outliers(data, column):
     mean = data[column].mean()
     median = data[column].median()
     mode = data[column].mode()
     std = data[column].std()
-    data[column] = data[column].mask(data[column] > mean + 3 * std)
+    plot_hist(data, column, xlabel, ylabel)
+    data = remove_outliers(data, column)
+    plot_hist(data, column, xlabel, ylabel)
+
+def plot_hist(data, column, xlabel, ylabel):
+    """
+    Plots a histogram (with xlabel and ylabel given by user)
+    of specific column of dataframe
+    """
+    data.hist(column)
+    plt.title(column, fontsize=16)
+    plt.xlabel(xlabel, fontsize=14)
+    plt.ylabel(ylabel, fontsize=14)
+    plt.show()
+
+def remove_outliers(data, column):
+    """
+    Removes outliers of a specific column of dataframe
+    """
+    data[column] = data[column].mask(data[column] > data[column].mean() + 3 * data[column].std())
     data = data.dropna(how='any')
     return data
 
 def boxplot(data, column):
+    """
+    Makes one boxplot with all observations of a column in dataframe
+    and one ordered by region
+    """
     data.boxplot(column, grid=False)
     data.boxplot(column, by="Region", rot=90, grid=False)
     plt.show()
-    return
 
 def make_json(data):
+    """
+    Coverts dataframe to JSON file with as index the names of the country
+    """
     data.set_index("Country").to_json("eda.json", orient="index")
-    return
 
 
 if __name__ == "__main__":
-    data_frame = csv_reader(INPUT_CSV)
+
+    # reads certain columns of CSV file
+    data_frame = csv_reader(INPUT_CSV, ["Country", "Region", "GDP ($ per capita) dollars",
+                 "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)"])
+
+    # preprocces dataframe
     data_frame = preprocessing(data_frame)
-    central_tendency(data_frame, "GDP ($ per capita) dollars", "Dollars")
-    data_frame = remove_outliers(data_frame, "Infant mortality (per 1000 births)")
+
+    # central tendency for GDP
+    central_tendency(data_frame, "GDP ($ per capita) dollars", "Dollars", "Frequency")
+
+    # boxplot of infant mortality
     boxplot(data_frame, "Infant mortality (per 1000 births)")
+
+    # convert dataframe to JSON file
     make_json(data_frame)
