e2cca766a48cd7744da2733d0186d54966a24b70
JRMfer <43999123+JRMfer@users.noreply.github.com>
2018-11-22 00:07:25 +0100

    final version

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # Name: Julien Fer
 # Student number: 10649441
+#
+# This program reads a csv file and convert it to a JSON file.
 
 import csv
 import pandas as pd
-import os
 import json
 
 # global variable for CSV file
-INPUT_CSV1 = "unemployment.csv"
-INPUT_CSV2 = "cpi.csv"
+INPUT_CSV = "unemployment.csv"
 
 def csv_reader(filename, columns):
     """
@@ -21,24 +21,15 @@ def csv_reader(filename, columns):
     data = data[columns]
     return data
 
-def merge_dataframe(file1, file2, id, how):
-    merged = file1.merge(file2, on=id, how=how)
-    merged.to_csv("data.csv", index=False)
-    return merged
 
 def make_json(data):
     """
-    Coverts dataframe to JSON file with as date as index
+    Coverts dataframe to JSON file with date as index
     """
     data.set_index("DATE").to_json("data.json", orient="index")
 
 
 if __name__ == "__main__":
 
-    data1 = csv_reader(INPUT_CSV1, ["DATE", "UNEMPLOYMENT"])
-    print(data1)
-    data2 = csv_reader(INPUT_CSV2, ["DATE", "CPI" ])
-    print(data2)
-    data3 = merge_dataframe(data1, data2, "DATE", "outer")
-    print(data3[["CPI"][0]])
-    make_json(data3)
+    data = csv_reader(INPUT_CSV, ["DATE", "UNEMPLOYMENT"])
+    make_json(data)
