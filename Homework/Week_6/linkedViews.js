26b2f54bbecddfb601b1994a733211284767d7c2
JRMfer <43999123+JRMfer@users.noreply.github.com>
2018-12-16 23:10:03 +0100

    linked view

--- Homework/Week_6/linkedViews.js
+++ Homework/Week_6/linkedViews.js
@@ -18,7 +18,7 @@ function title(txt) {
 
 function addParagraph(txt, id) {
   // add paragraph to body
-  d3.select("body")
+  d3.select("#area3")
     .append("p")
     .attr("class", "paragraph")
     .attr("id", id)
@@ -27,7 +27,7 @@ function addParagraph(txt, id) {
 
 function addLink(txt, url) {
   // add links to body
-  d3.select("body")
+  d3.select("#area3")
     .append("p")
     .attr("class", "paragraph")
     .attr("id", "link")
@@ -42,7 +42,7 @@ var spendingGovernment =  "Government_spending_2014.json"
 var worldCountries = "http://bl.ocks.org/micahstubbs/raw/8e15870eb432a21f0bc4d3d527b2d14f/a45e8709648cafbbf01c78c76dfa53e31087e713/world_countries.json"
 var euCountries = "https://gist.githubusercontent.com/milafrerichs/69035da4707ea51886eb/raw/4cb1783c2904f52cbb8a258ee96031f9054d155b/eu.topojson"
 
-// global constant margins
+// global constants margins and dimensions for datamap and barchart
 var marginsMap = {
   "left": 100,
   "right": 100,
@@ -73,27 +73,9 @@ var dimBar =  {
 // this function assures that the following is executed when the page is load
 window.onload = function() {
 
-  // add title, a few paragraphs and links to datasets to the body
-  title("Government spending 2014");
-  addParagraph("Name: Julien Fer", "name");
-  addParagraph("Studentnumber: 10649441", "studentnumber");
-  addLink("Dataset: Central government spending", governmentSpending);
-  addParagraph("This page shows the central goverment spending in (2014) for countries in the OECD. In the map of Europe one sees ,while hovering over a country, the sum of government spending on 5 categorires as a percentage on the total goverment spending. When you click on the country the barchart will show the percentage of spending in the following 5 categories: ", "explanation");
-  var names = ["Economic Affairs (ECOAFF)", "Education (EDU)", "Health (Health)", "Housing and community amenities (HOUCOMM)", "Social protection (SOCPROT)"];
-
-	var ul = d3.select('#explanation').append('ul');
-
-	ul.selectAll('li')
-	.data(names)
-	.enter()
-	.append('li')
-	.html(function(d) {
-    return d;
-  });
-
   var format = d3.format(".1f");
 
-  // Set tooltips
+  // Set tooltips for datamap
   var tip = d3.tip()
             .attr('class', 'd3-tip')
             .offset([100, 100])
@@ -112,6 +94,7 @@ window.onload = function() {
                 }
               });
 
+  // set svg for datamap
   var svg = d3.select("#area1")
               .append("svg")
               .attr("height", dimMap.height + marginsMap.top + marginsMap.bottom)
@@ -120,11 +103,11 @@ window.onload = function() {
               .attr("id", "map")
               .append("g")
               .attr("transform", "translate(" + marginsMap.left + "," + marginsMap.top  + ")");
-
   svg.call(tip);
 
-let scores = [10, 20, 30, 40, 50, 60, 70, 80];
-let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(161,217,155)", "rgb(116,196,118)", "rgb(65,171,93)","rgb(35,139,69)","rgb(0,90,50)"];
+  // set scale for colors in datamap with threshold every 10%
+  let scores = [10, 20, 30, 40, 50, 60, 70, 80];
+  let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(161,217,155)", "rgb(116,196,118)", "rgb(65,171,93)","rgb(35,139,69)","rgb(0,90,50)"];
 
   let color = d3.scaleThreshold()
     .domain(scores)
@@ -138,13 +121,35 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
   var requests = [d3.json(euCountries), d3.json(spendingGovernment)]
 
   Promise.all(requests).then( function(response) {
+    // gather data
     let topology = response[0];
     let data = response[1];
     dimBar["data"] = data;
-    console.log(topology);
-    console.log(dimBar["data"]);
+
+    // make datamap and barChart
     ready(0, topology, data);
     drawBarChart("#area2", marginsBar, dimBar, "barChart", "OECD");
+
+    // add title, a few paragraphs and link to dataset to the body
+    title("Government spending 2014");
+    addParagraph("This page shows the central goverment spending in (2014) for countries in the OECD. In the map of Europe one sees ,while hovering over a country, the sum of government spending on 5 categorires as a percentage on the total goverment spending. When you click on the country the barchart will show the percentage of spending in the following 5 categories: ", "explanation");
+    var names = ["Economic Affairs (ECOAFF)", "Education (EDU)", "Health (Health)", "Housing and community amenities (HOUCOMM)", "Social protection (SOCPROT)"];
+
+    // append bullet points to paragraph with explanation
+    var ul = d3.select('#explanation').append('ul');
+
+    ul.selectAll('li')
+    .data(names)
+    .enter()
+    .append('li')
+    .html(function(d) {
+      return d;
+    });
+
+    // add name, studentnumber and link to dataset
+    addParagraph("Name: Julien Fer", "name");
+    addParagraph("Studentnumber: 10649441", "studentnumber");
+    addLink("Dataset: Central government spending", governmentSpending);
   }).catch( function(e) {
     throw (e);
   });
@@ -174,21 +179,18 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
     */
 
     var countries = topojson.feature(data, data.objects.europe).features
-    console.log(countries);
-    console.log(spending);
 
-
-    /*
-      Add a path for each country
-    */
-
+    // add a path for each country
     svg.selectAll(".country")
       .data(countries)
       .enter().append("path")
       .attr("class", "country")
       .attr("d", path)
+      // fill color country according the scaled threshold array for colors
       .attr("fill", function(d){
         let shortName = d.properties.iso_a3;
+
+        // checks if country contains data, if not fill country with color grey
         if (spending[shortName]) {
           let keys = Object.keys(spending[shortName]);
           let total = 0
@@ -198,9 +200,10 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
           return color(total);
           }
           else {
-            return "rgb(0, 0, 0)";
+            return "rgb(64, 64, 64)";
           }
       })
+      // set mouseover effect with tooltip
       .on("mouseover", function(d) {
         d3.select(this).classed("selected", true);
         tip.show(d, spending);
@@ -209,11 +212,13 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
         d3.select(this).classed("selected", false);
         tip.hide(d, spending);
       })
+      // when country is clicked update function is called
       .on("click", function(d) {
         console.log(d.properties.iso_a3);
         return updateGraph(dimBar, d.properties.iso_a3, marginsBar)
       })
 
+      // append legend to datamap
       var legend = svg.selectAll(".legend")
           .data(scores)
           .enter().append('g')
@@ -224,6 +229,7 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
           }
       })
 
+      // append rectangles to legend
       legend.append('rect')
           .attr("x", 0)
           .attr("y", 250)
@@ -233,10 +239,10 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
           return color(d - 1);
       })
 
+      // set the corresponding values in legend
       legend.append('text')
           .attr("x", 30)
           .attr("y", 265)
-      //.attr("dy", ".35em")
       .text(function (d, i) {
           return "< " + d + "%";
       })
@@ -246,6 +252,8 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
   }
 
   function findMaxObject(data) {
+    // find maximum value of one-dimensional object
+
     let keys = Object.keys(data);
     let maxValue = 0;
     keys.forEach( function(key) {
@@ -257,6 +265,8 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
   }
 
   function findMinObject(data) {
+    // finds minimum value of one-dimensional object
+
     let keys = Object.keys(data);
     let minValue = 0;
     keys.forEach( function(key) {
@@ -275,6 +285,8 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
 
 
   function drawBarChart(area, margins, dim, id, country) {
+    // draws a bar chart
+
     // set up svg element
     var svg = d3.select(area)
       .append("svg")
@@ -283,21 +295,18 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
       .attr("width", dim.width)
       .attr("height", dim.height);
 
-    // set yScale graph
+    // set yScale barchart
     var yScale = d3.scaleLinear()
       .domain([0, findMaxObject(dim.data[country])])
       .range([dim.height - margins.top, margins.bottom]);
 
-    console.log(yScale(0));
-    console.log(yScale(findMaxObject(dim.data[country])));
-    keys = Object.keys(dim.data[country]);
-
     // set xScale graph
+    keys = Object.keys(dim.data[country]);
     var xScale = d3.scaleLinear()
       .domain([0, keys.length])
       .range([margins.left, dim.width - margins.right]);
 
-    // Set tooltips
+    // Set tooltip for barchart
     var tip = d3.tip()
               .attr('class', "d3-tip")
               .offset([0, 0])
@@ -328,6 +337,7 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
       .attr("width", (dim.width - margins.left - margins.right) / keys.length -
         dim.barPadding)
       .attr("height", 0)
+      // add mouseover effects bars
       .on("mouseover", function(d) {
         d3.select(this).classed("selected", true);
         tip.show(d, country);
@@ -361,6 +371,7 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
       .tickValues(keys)
       .scale(xAxisScale);
 
+    // set x axis at appropriate place
     svg.append("g")
       .attr("class", "axis")
       .attr("id", "xAxis")
@@ -368,7 +379,6 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
                                             ) + ")")
       .transition()
       .duration(dim.animateDuration)
-      .ease(d3.easeLinear)
       .call(xAxis)
       .selectAll("text")
       .attr("transform", "rotate(-65)");
@@ -384,7 +394,7 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
       .attr("transform", "translate(" + margins.left + ",0)")
       .transition()
       .duration(dim.animateDuration)
-      .ease(d3.easeLinear)
+      // .ease(d3.easeLinear)
       .call(yAxis);
 
     // create Title
@@ -413,13 +423,16 @@ let colors = ["rgb(247,252,245)", "rgb(229,245,224)", "rgb(199,233,192)", "rgb(1
   }
 
   function updateGraph(dim, country, margins) {
-    // removes datapoints old barChart
-    if (dim.data[country]) {
-      d3.select("#barChart").remove();
-      console.log(dim);
-      console.log(country);
-      console.log(margins);
+    // updates barchart according to the country selected
 
+    // checks if country contains data
+    if (dim.data[country]) {
+
+      // removes svg containing the barChart
+      d3.select("#barChart")
+      .remove();
+
+      // returns function to draw barchart with selected country
       return drawBarChart("#area2", margins, dim, "barChart", country);
     }
   }
